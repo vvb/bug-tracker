ea8e44d53ab5ddda6a070b93dcb11e5d46c7c39b
vvb <vvb@cisco.com>
2017-02-21 16:00:40 +0530

    adding customer imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BugTrackerComponent } from './bug-tracker/bugTracker.component';
+import { GetClosedCountPipe } from './bug-tracker/pipes/getClosedCount.pipe';
+import { TrimTextPipe } from './bug-tracker/pipes/trimText.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BugTrackerComponent,
+    GetClosedCountPipe,
+    TrimTextPipe
   ],
   imports: [
     BrowserModule,
